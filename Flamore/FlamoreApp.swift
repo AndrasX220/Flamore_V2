0d6547ec73b13b7f036a403724e93f6ce1c2a29c
andrasX220 <handras@kapulan.hu>
2024-12-17 16:34:30 +0100

    Initial Commit

---
+++ Flamore/FlamoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlamoreApp.swift
+//  Flamore
+//
+//  Created by Hoffer Andras on 2024. 12. 17..
+//
+
+import SwiftUI
+
+@main
+struct FlamoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlamoreTests/FlamoreTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlamoreTests.swift
+//  FlamoreTests
+//
+//  Created by Hoffer Andras on 2024. 12. 17..
+//
+
+import Testing
+@testable import Flamore
+
+struct FlamoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
